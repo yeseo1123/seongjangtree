79ab5135bd92df8a3f5ec84402499d8a98dcb644
JU YESEO <protojunseo@naver.com>
2026-06-04 01:05:36 +0900

    Create student.h

---
+++ include/student.h
@@ -0,0 +1,29 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#define NAME_LEN 50
+
+typedef struct Student {
+    int id;
+    char name[NAME_LEN];
+
+    int korean;
+    int english;
+    int math;
+
+    float average;
+
+    struct Student* next;
+} Student;
+
+Student* createStudent(
+    int id,
+    char* name,
+    int kor,
+    int eng,
+    int math);
+
+void printStudent(
+    Student* student);
+
+#endif

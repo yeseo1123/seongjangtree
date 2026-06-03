58cf649d92417d4d23e4a6328593c0ffb3caaa64
JU YESEO <protojunseo@naver.com>
2026-06-04 01:06:58 +0900

    Create student.c

---
+++ src/student.c
@@ -0,0 +1,17 @@
+// student.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "student.h"
+
+Student* createStudent(
+    int id,
+    char* name,
+    int kor,
+    int eng,
+    int math)
+{
+    ...
+}

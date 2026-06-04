476b0cf4c61fbd3e77753e9c4a3f2d7746f0a711
JU YESEO <protojunseo@naver.com>
2026-06-04 11:25:30 +0900

    Create menu.c

---
+++ src/menu.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "menu.h"
+
+void showMenu()
+{
+    printf("\n");
+    printf("SeongJangTree\n");
+    printf("1. 학생 등록\n");
+    printf("2. 학생 조회\n");
+    printf("3. 반 평균 계산\n");
+    printf("4. 우수 학생 조회\n");
+    printf("5. 위험 학생 조회\n");
+    printf("0. 종료\n");
+    printf("선택 : ");
+}

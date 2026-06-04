c7d42a198ec8c6bdf04a001842fe1774d5f764ee
JU YESEO <protojunseo@naver.com>
2026-06-04 11:18:33 +0900

    Update student.c

--- src/student.c
+++ src/student.c
@@ -1,17 +1,51 @@
-// student.c
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#include "student.h"
+#include "hashmap.h"
 
-Student* createStudent(
-    int id,
-    char* name,
-    int kor,
-    int eng,
-    int math)
+HashNode* hashTable[TABLE_SIZE] = { NULL };
+
+int hashFunction(int key)
 {
-    ...
+    return key % TABLE_SIZE;
+}
+
+void insertHash(Student* student)
+{
+    int index = hashFunction(student->id);
+
+    HashNode* newNode =
+        (HashNode*)malloc(sizeof(HashNode));
+
+    if (newNode == NULL)
+    {
+        printf("메모리 할당 실패\n");
+        return;
+    }
+
+    newNode->key = student->id;
+    newNode->student = student;
+
+    newNode->next = hashTable[index];
+    hashTable[index] = newNode;
+}
+
+Student* searchHash(int id)
+{
+    int index = hashFunction(id);
+
+    HashNode* current =
+        hashTable[index];
+
+    while (current != NULL)
+    {
+        if (current->key == id)
+        {
+            return current->student;
+        }
+
+        current = current->next;
+    }
+
+    return NULL;
 }

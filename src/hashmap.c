25abebd9be26023f872bd2b845b4a24a0cf0087c
JU YESEO <protojunseo@naver.com>
2026-06-04 11:41:51 +0900

    Update hashmap.c

--- src/hashmap.c
+++ src/hashmap.c
@@ -1,4 +1,9 @@
-// hashmap.c
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "hashmap.h"
+
+HashNode* hashTable[TABLE_SIZE] = { NULL };
 
 int hashFunction(int key)
 {
@@ -7,10 +12,40 @@ int hashFunction(int key)
 
 void insertHash(Student* student)
 {
-    ...
+    int index = hashFunction(student->id);
+
+    HashNode* newNode =
+        (HashNode*)malloc(sizeof(HashNode));
+
+    if (newNode == NULL)
+    {
+        printf("메모리 할당 실패\n");
+        return;
+    }
+
+    newNode->key = student->id;
+    newNode->student = student;
+
+    newNode->next = hashTable[index];
+    hashTable[index] = newNode;
 }
 
 Student* searchHash(int id)
 {
-    ...
+    int index = hashFunction(id);
+
+    HashNode* current =
+        hashTable[index];
+
+    while (current != NULL)
+    {
+        if (current->key == id)
+        {
+            return current->student;
+        }
+
+        current = current->next;
+    }
+
+    return NULL;
 }

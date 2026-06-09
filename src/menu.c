1386e944e10d3935fb5df6f3ebf8d7260c63272e
JU YESEO <protojunseo@naver.com>
2026-06-09 20:52:41 +0900

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -9,7 +9,7 @@ void showMenu()
     printf("1. 학생 등록\n");
     printf("2. 학생 조회\n");
     printf("3. 학생 성적 수정\n");
-    printf("4. 우수 학생 조회\n");
+    printf("4. 반 평균 계산\n");
     printf("5. 위험 학생 조회\n");
     printf("0. 종료\n");
     printf("선택 : ");

bfbc3dca6e338053a43682eabf0299ff442224d2
JU YESEO <protojunseo@naver.com>
2026-06-09 20:43:54 +0900

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -8,7 +8,7 @@ void showMenu()
     printf("SeongJangTree\n");
     printf("1. 학생 등록\n");
     printf("2. 학생 조회\n");
-    printf("3. 반 평균 계산\n");
+    printf("3. 학생 성적 수정\n");
     printf("4. 우수 학생 조회\n");
     printf("5. 위험 학생 조회\n");
     printf("0. 종료\n");
